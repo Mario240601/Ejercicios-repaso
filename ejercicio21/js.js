648eb26d34883fa724fe110f96c7e3e734b15aa0
Mario240601 <92162524+Mario240601@users.noreply.github.com>
2022-02-18 14:14:38 +0100

    Add files via upload

---
+++ ejercicio21/js.js
@@ -0,0 +1,7 @@
+function alerta() {
+ var info = document.getElementsByTagName('input')[0].value;
+ alert (info);
+ var long = document.getElementsByTagName('input')[0].value.length;
+ alert(long);
+}
+ 
